fb10da4839384269c0dedf834aa3be111efc0e65
苏文杰 <swj1996a@163.com>
2019-07-19 09:18:28 +0800

    修改Progress.cs错误代码

--- PersonInfoManage/PersonInfoManage/Program.cs
+++ PersonInfoManage/PersonInfoManage/Program.cs
@@ -18,11 +18,11 @@ namespace PersonInfoManage
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new MainForm());
             //Application.Run(new LoginForm());
-            //Application.Run(new PersonDetail());
-            //Application.Run(new UpdatFileName());
-            //Application.Run(new CostApplyDetail());
+            //Application.Run(new PersonDetailForm());
+            //Application.Run(new UpdatFileNameForm());
+            //Application.Run(new CostApplyDetailForm());
 
-            Application.Run(new CostPlan());
+            Application.Run(new CostPlanForm());
         }
     }
 }
