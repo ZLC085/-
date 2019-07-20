5f319ad2bd68354c7f0335cebd2169356b94650d
曾丽川 <2971146011@qq.com>
2019-07-20 16:31:21 +0800

    用户组查询更新

--- PersonInfoManage/PersonInfoManage.Test/Program.cs
+++ PersonInfoManage/PersonInfoManage.Test/Program.cs
@@ -49,11 +49,14 @@ namespace PersonInfoManage.Test
             //Perm perm = new Perm();
             //Console.WriteLine(perm.Selectall());
 
-            //测试函数：perm.Selectgroup(group) 测试成功
             //PermDAL perm = new PermDAL();
             //sys_group group = new sys_group();
             //group.group_name = "管理员";
+            //DateTime a = new DateTime(2011,1,1,0,0,0);
+            //group.create_time = a;
+            //group.modify_time = new DateTime(2022,1,1,1,1,1);
             //Console.WriteLine(perm.Selectgroup(group));
+
 
             //测试函数：SysUser.add(sys_user user,int groupid)  测试成功
             //SysUser sysuser = new SysUser();
