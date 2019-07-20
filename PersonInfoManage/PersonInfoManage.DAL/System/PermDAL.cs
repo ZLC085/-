a98738acea665e8f209482cf424d9613389c6be9
王尔沛 <873257742@qq.com>
2019-07-20 11:14:58 +0800

    更新用户管理BLL

--- PersonInfoManage/PersonInfoManage.DAL/System/PermDAL.cs
+++ PersonInfoManage/PersonInfoManage.DAL/System/PermDAL.cs
@@ -13,7 +13,7 @@ namespace PersonInfoManage.DAL.System
     /// <summary>
     /// 权限管理
     /// </summary>
-    public class PermDAL :DALBase
+    public class PermDAL : DALBase
     {
         /// <summary>
         ///添加用户组
@@ -27,7 +27,7 @@ namespace PersonInfoManage.DAL.System
             string sql1 = "Insert into sys_group(group_name,remark,create_time,modify_time) values(@p1,@p2,getdate(),getdate())";
             SqlParameter sqlparameter1 = new SqlParameter("@p1", group.group_name);
             SqlParameter sqlparameter2 = new SqlParameter("@p2", group.remark);
-            res = SqlHelper.ExecuteNonQuery(ConStr, CommandType.Text, sql1, sqlparameter1, sqlparameter2);                  
+            res = SqlHelper.ExecuteNonQuery(ConStr, CommandType.Text, sql1, sqlparameter1, sqlparameter2);
             return res;
 
         }
@@ -39,13 +39,13 @@ namespace PersonInfoManage.DAL.System
         /// <param name="user_id">用户id</param>
         /// <param name="group_id">用户组id</param>
         /// <returns>返回添加条数</returns>
-        public int add(int user_id,int group_id)
+        public int add(int user_id, int group_id)
         {
             int res;
             string sql = "insert into sys_u2g (user_id,group_id) values(@p1,@p2)";
             SqlParameter sqlparameter = new SqlParameter("@p1", user_id);
             SqlParameter sqlparameter1 = new SqlParameter("@p2", group_id);
-            res=SqlHelper.ExecuteNonQuery(ConStr, CommandType.Text, sql, sqlparameter, sqlparameter1);
+            res = SqlHelper.ExecuteNonQuery(ConStr, CommandType.Text, sql, sqlparameter, sqlparameter1);
             return res;
         }
 
@@ -56,43 +56,17 @@ namespace PersonInfoManage.DAL.System
         /// <param name="id">id</param>
         /// <param name="newValues">需要修改的值</param>
         /// <returns>修改条数</returns>
-        public int Update(sys_group group, List<string> grouplist)
+        public int Updateg2m(int menu_id, int group_id)
         {
-            using (SqlConnection connection = new SqlConnection(ConStr))
-            {
-                SqlConnection conn = new SqlConnection(ConStr);
-                SqlCommand command = new SqlCommand();
-                SqlTransaction trans = null;
-                DataSet ds = new DataSet();
-                sys_menu menu = new sys_menu();
-                try
-                {
-                    conn.Open();
-                    trans = conn.BeginTransaction();
-                    command.Transaction = trans;
-                    command.Connection = conn;
-                    string sql = "select * from sys_menu where menu_name=@p1";
-                    string sql1 = "Insert into sys_g2m(group_id,menu_id) values(@p2,@p3)";
-                    foreach (var name in grouplist)
-                    {
-                        SqlParameter sqlParameter = new SqlParameter("@p1", name);
-                        ds = SqlHelper.ExecuteDataset(connection, CommandType.Text, sql, sqlParameter);
-                        menu.id = (int)ds.Tables[0].Rows[0][nameof(sys_menu.id)];
-                        SqlParameter sqlParameter1 = new SqlParameter("@p2", group.id);
-                        SqlParameter sqlParameter2 = new SqlParameter("@p3", menu.id);
-                        SqlHelper.ExecuteNonQuery(connection, CommandType.Text, sql1, sqlParameter1, sqlParameter2);
-                    }
-                    trans.Commit();
-                    return 1;
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                    trans.Rollback();
-                    return 0;
-                }
-            }
+
+            int res;
+            string sql1 = "Insert into sys_g2m(group_id,menu_id) values(@p1,@p2)";
+            SqlParameter sqlParameter1 = new SqlParameter("@p1", group_id);
+            SqlParameter sqlParameter2 = new SqlParameter("@p2", menu_id);
+            res = SqlHelper.ExecuteNonQuery(ConStr, CommandType.Text, sql1, sqlParameter1, sqlParameter2);
+            return res;
         }
+    
 
 
         /// <summary>
@@ -101,7 +75,7 @@ namespace PersonInfoManage.DAL.System
         /// <param name="user_id">用户id</param>
         /// <param name="group_id">用户组id</param>
         /// <returns>返回添加条数</returns>
-        public int Update(int user_id, int group_id)
+        public int Updateu2g(int user_id, int group_id)
         {
             int res;
             string sql = "update sys_u2g SET group_id= '"+group_id+"' where user_id= '" + user_id + "'";
