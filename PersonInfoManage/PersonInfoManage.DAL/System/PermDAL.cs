0f393e56493cba42506f92ff3f65dc4c1e130e23
王尔沛 <873257742@qq.com>
2019-07-23 11:05:42 +0800

    修改权限管理方法和测试类

--- PersonInfoManage/PersonInfoManage.DAL/System/PermDAL.cs
+++ PersonInfoManage/PersonInfoManage.DAL/System/PermDAL.cs
@@ -33,7 +33,7 @@ namespace PersonInfoManage.DAL.System
 
 
         /// <summary>
-        /// 关联用户与用户组
+        /// 添加用户进用户组
         /// </summary>
         /// <param name="userId">用户id</param>
         /// <param name="groupId">用户组id</param>
@@ -85,15 +85,14 @@ namespace PersonInfoManage.DAL.System
 
 
         /// <summary>
-        /// 删除用户组和权限关联
+        /// 删除用户组的权限
         /// </summary>
         /// <param name="groupId">用户组id</param>
-        /// <param name="menuId">权限id</param>
         /// <returns>删除条数</returns>
-        public int DelG2m(int groupId,int menuId)
+        public int DelG2m(int groupId)
         {
             int res = 0;
-            string sql = "Delete from sys_g2m where group_id='" + groupId + "' and menu_id='" + menuId + "'";
+            string sql = "Delete from sys_g2m where group_id='" + groupId + "'";
             res = SqlHelper.ExecuteNonQuery(ConStr, CommandType.Text, sql);
             return res;
         }
@@ -150,7 +149,7 @@ namespace PersonInfoManage.DAL.System
         }
 
         /// <summary>
-        /// 删除用户组和用户关联
+        /// 删除用户中的用户
         /// </summary>
         /// <param name="userId">用户id</param>
         /// <param name="groupId">用户id</param>
