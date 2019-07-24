336c231d3f82cd2c0bf3b0332ba57da4d75eaefb
李鸽鸽 <lg15594617691@outlook.com>
2019-07-24 11:31:36 +0800

    01

---
+++ PersonInfoManage/PersonInfoManage/PersonInfo/UpdateFileName.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PersonInfoManage.PersonInfo
+{
+    public partial class UpdateFileName : Form
+    {
+        public UpdateFileName()
+        {
+            InitializeComponent();
+        }
+
+        private void LblFileName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            SqlConnection con;
+            con = new SqlConnection("server = (47.106.235.197); database = localhost; uid = SA; pwd = qwer-123456");
+            con.Open();
+
+            String find = "select * from person_file where filename = ' " + filename + " '";
+            SqlDataAdapter myDa = new SqlDataAdapter(find, con);
+            DataSet Ds = new DataSet();
+            myDa.Fill(Ds, "name");
+            DataGridView1_CellContentClick.DataSourse = Ds.Tables["name"];
+
+        }
+    }
+}
