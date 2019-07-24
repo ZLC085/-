c8d63f315a6f73603475142bad56c1b2f901c95a
陈波 <calranger@163.com>
2019-07-24 11:54:32 +0800

    CostApplyDetailForm_Load方法提交

--- PersonInfoManage/PersonInfoManage/Cost/CostApplyDetailForm.cs
+++ PersonInfoManage/PersonInfoManage/Cost/CostApplyDetailForm.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using PersonInfoManage.Model;
 using PersonInfoManage.BLL.Cost;
+using PersonInfoManage.DAL.System;
 
 namespace PersonInfoManage
 {
@@ -20,7 +21,10 @@ namespace PersonInfoManage
             this.costId = costId;
             InitializeComponent();
         }
-
+        public CostApplyDetailForm()
+        {
+            InitializeComponent();
+        }
         private void CostApplyDetailForm_Load(object sender, EventArgs e)
         {
             cost cost = new CostApplyBLL().Query(new Dictionary<string, object>
@@ -29,11 +33,16 @@ namespace PersonInfoManage
             }).First();
             cost_main main = cost.main;
             LblCostId.Text = main.id.ToString();
-            //申请人
-            //所属部门
+            view_sys_u2g applicant = new SysUserDAL().SelectById(main.apply_id).First();
+            LblApplicant.Text = applicant.name;
+            LblApplicant.Text = applicant.org_name;
             LblApplyTime.Text = main.apply_money.ToString();
             LblApplyMoney.Text = main.apply_money.ToString();
-            //申请明细
+            foreach(cost_detail detail in cost.DetailList)
+            {
+                int index = this.DgvCostDetail.Rows.Add();
+                this.DgvCostDetail.Rows[index].SetValues(detail.cost_type_name, detail.money);
+            }
             switch (main.status)
             {
                 case 0:LblStatus.Text = "未审核";break;
@@ -41,13 +50,11 @@ namespace PersonInfoManage
                 case 2: LblStatus.Text = "审核通过"; break;
                 case 3: LblStatus.Text = "审核驳回"; break;
             }
-            //审批人
-            //所属部门
+            view_sys_u2g approver = new SysUserDAL().SelectById(main.approval_id).First();
+            LblApprover.Text = approver.name;
+            LblApproverOrg.Text = approver.org_name;
             LblApprovalMoney.Text = main.approval_money.ToString();
-            LblRemark.Text = main.remark;
-
-
-            
+            LblRemark.Text = main.remark;           
         }
     }
 }
