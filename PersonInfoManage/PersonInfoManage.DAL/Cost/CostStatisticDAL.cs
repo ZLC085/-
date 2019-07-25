accd5b2c0d0a9bd1f4bda6a80baf70d73324b760
陈波 <calranger@163.com>
2019-07-25 10:05:41 +0800

    更新费用管理DAL、BLL层代码

--- PersonInfoManage/PersonInfoManage.DAL/Cost/CostStatisticDAL.cs
+++ PersonInfoManage/PersonInfoManage.DAL/Cost/CostStatisticDAL.cs
@@ -21,22 +21,9 @@ namespace PersonInfoManage.DAL.Cost
         /// <returns>费用信息列表</returns>
         public List<cost> Query(Dictionary<string, object> conditions)
         {
-            List<cost> costList = new List<cost>();
             conditions.Add("status", 2);
-            CostApplyDAL apply = new CostApplyDAL();
-            List<cost_main> listMain = apply.QueryMain(conditions);
-            foreach( cost_main main in listMain)
-            {
-                List<cost_detail> listDetail = apply.QueryDetail(main.id);
-                cost cost = new cost
-                {
-                    main = main,
-                    DetailList = listDetail
-                };
-                costList.Add(cost);
-            }
+            List<cost> costList = new CostApprovaDAL().Query(conditions);            
             return costList;
         }
-
     }
 }
